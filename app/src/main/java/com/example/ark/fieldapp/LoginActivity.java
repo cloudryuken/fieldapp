d0b616ddf86fc8045fab76bb30c151802b4744c7
Ark <ark@eclipse.code>
2017-04-13 17:23:46 +0300

    update

--- app/src/main/java/com/example/ark/fieldapp/LoginActivity.java
+++ app/src/main/java/com/example/ark/fieldapp/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.example.ark.fieldapp;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,6 +31,44 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                String name = jsonResponse.getString("name");
+                                String email = jsonResponse.getString("email");
+
+                                Intent intent = new Intent(LoginActivity.this, UserActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("username", username);
+                                intent.putExtra("email", email);
+
+                                LoginActivity.this.startActivity(intent);
+                            }else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
             }
         });
     }
--- app/src/main/java/com/example/ark/fieldapp/LoginRequest.java
+++ app/src/main/java/com/example/ark/fieldapp/LoginRequest.java
@@ -7,17 +7,15 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterRequest extends StringRequest {
-    private static final String REGISTER_REQUEST_URL = "https://redcloud.000webhostapp.com/register.php";
+public class LoginRequest extends StringRequest{
+    private static final String LOGIN_REQUEST_URL = "https://redcloud.000webhostapp.com/login.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String username, String password, String name, String email, Response.Listener<String> listener) {
-        super(Request.Method.POST, REGISTER_REQUEST_URL, listener, null);
+    public LoginRequest(String username, String password, Response.Listener<String> listener) {
+        super(Request.Method.POST, LOGIN_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("username", username);
         params.put("password", password);
-        params.put("name", name);
-        params.put("email", email);
     }
 
     @Override
